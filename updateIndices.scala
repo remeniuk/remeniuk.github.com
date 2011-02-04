f63fa60efc48e692123188630299f988cd22144c
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-02-04 17:16:57 +0200

    added indices

---
+++ updateIndices.scala
@@ -0,0 +1,28 @@
+import java.io._
+
+val LOCAL_REPO = "D:/dropbox/My Dropbox/remeniuk.github.com/" 
+val IGNORE_FILE = List(".git", "index.html", "updateIndices.bat", "updateIndices.scala")
+
+def updateIndex(currentDir: String = ""):Unit = {
+	val currentPath = LOCAL_REPO + currentDir
+	println("Updating indeces in " + currentPath)
+	val loc = new File(currentPath)
+	if(loc.isDirectory){
+		loc.listFiles.find(_.getName == "index.html").
+			map(_.delete)
+		new File(currentPath + "/index.html").createNewFile
+		val out = new java.io.FileWriter(currentPath + "/index.html")
+		out.write(
+			(<html>
+				<h1>Maven Repository remeniuk@github</h1>
+				<ul>{loc.listFiles.filterNot(file => IGNORE_FILE.contains(file.getName))
+				.map(file => <li><a href={"http://remeniuk.github.com" + currentDir +  "/" + file.getName}>{
+				(if(file.isDirectory) "DIRECTORY -- " else "FILE -- ") + file.getName}</a></li>)}</ul> 
+			</html>) toString
+		)
+		out.close
+		loc.listFiles.filter(_.isDirectory).filterNot(file => IGNORE_FILE.contains(file.getName)).foreach(dir => updateIndex(currentDir + "/" + dir.getName))
+	}
+}
+
+updateIndex()
