ecf5e3afeae56e6d6d0b5dd877c1809e9e00a139
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-04-23 21:46:45 +0300

    Added sbt-netbeans-plugin-0.0.3

--- update-all-index.sh
+++ update-all-index.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for i in $(find maven -type d); do
-   (cd $i; pwd; "/d/My Dropbox/remeniuk.github.com/update-index.sh" > index.html)
+	(cd $i; pwd; "/home/remeniuv/Dropbox/remeniuk.github.com/update-index.sh" > index.html)
 done
