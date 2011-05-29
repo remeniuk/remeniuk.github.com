606679e2535bc214b80f2ac708f46c7582745b2e
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-05-29 10:02:04 +0300

    added v0.0.10 compatible with scala 2.9.0-1 and sbt 0.9.9

--- update-all-index.sh
+++ update-all-index.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for i in $(find maven -type d); do
-	(cd $i; pwd; /d/project/remeniuk.github.com/update-index.sh > index.html)
+	(cd $i; pwd; /home/remeniuv/remeniuk.github.com/update-index.sh > index.html)
 done
