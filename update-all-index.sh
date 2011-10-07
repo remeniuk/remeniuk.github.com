5bcb29d479675d63bf7892b5d1ea841dbba5fd35
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2011-10-07 15:05:55 +0300

    added repo layout for netbeans plugin v.0.1.3

--- update-all-index.sh
+++ update-all-index.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for i in $(find maven -type d); do
-	(cd $i; pwd; /home/remeniuv/remeniuk.github.com/update-index.sh > index.html)
+	(cd $i; pwd; /home/remeniuv/projects/remeniuk.github.com/update-index.sh > index.html)
 done
